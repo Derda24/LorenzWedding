f4de1051eb37178e202d1c012b252899fa1709e9
Vedatturlu <vturlu.dpointgroup@gmail.com>
2026-02-22 14:57:37 +0300

    About: add fourth team member slot in admin and data

--- push-to-github.bat
+++ push-to-github.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "c:\Users\turlu\Lorenz Wedding"
 git add -A
-git commit -m "Admin: 4 featured photos + Hakkımızda editable from JSON; about.json and content.js"
+git commit -m "About: add fourth team member slot in admin and data"
 git push origin main
 pause
